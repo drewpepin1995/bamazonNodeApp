07b56c431d9952d7fad6f05b965cca48f89907bf
Drew Pepin <drewpepin1995@gmail.com>
2019-09-20 12:53:37 -0400

    setting up startApp function to initialize prompt

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,19 +3,39 @@ var mysql = require('mysql');
 var consoleTable = require('console.table');
 
 var connection = mysql.createConnection({
-	host: "localhost",
-	port: 3306,
-	user: "root",
-	password: "hockey930",
-	database: "bamazon"
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "hockey930",
+    database: "bamazon"
 });
 
-connection.connect(function(error){
-	if (error) throw error;
-	// welcome customer
-	console.log("\n-----------------------------------------------------------------" 
-		+ "\nWelcome to Bamazon! Check out what we've got for you!\n" 
-		+ "-----------------------------------------------------------------\n");
-	// start the app
-	// welcome();
+connection.connect(function (error) {
+    if (error) throw error;
+
+    console.log("\n-----------------------------------------------------------------"
+        + "\nWelcome to Bamazon! Check out what we've got for you!\n"
+        + "-----------------------------------------------------------------\n");
+
+    startApp();
+
 });
+
+function startApp() {
+    inquirer.prompt([
+        {
+            name: "action",
+            type: "list",
+            choices: ["View items for sale", "Leave the store"],
+            message: "Please select what you would like to do!"
+
+        }
+
+    ]).then(function(action){
+        if (action.action === "View items for sale") {
+            viewItems();
+        } else if (action.action === "Leave the store") {
+            stopApp();
+        }
+    })
+};
