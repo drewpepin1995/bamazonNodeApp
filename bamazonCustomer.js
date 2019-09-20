5621d6ce95b2bd491987e792b74b799bdf19d6cf
Drew Pepin <drewpepin1995@gmail.com>
2019-09-20 12:39:55 -0400

    Connecting to bamazon DB

---
+++ bamazonCustomer.js
@@ -0,0 +1,21 @@
+var inquirer = require('inquirer');
+var mysql = require('mysql');
+var consoleTable = require('console.table');
+
+var connection = mysql.createConnection({
+	host: "localhost",
+	port: 3306,
+	user: "root",
+	password: "hockey930",
+	database: "bamazon"
+});
+
+connection.connect(function(error){
+	if (error) throw error;
+	// welcome customer
+	console.log("\n-----------------------------------------------------------------" 
+		+ "\nWelcome to Bamazon! Check out what we've got for you!\n" 
+		+ "-----------------------------------------------------------------\n");
+	// start the app
+	// welcome();
+});
